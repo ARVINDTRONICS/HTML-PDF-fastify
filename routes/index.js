61172105b54af189481dde4b4431aa23c9442aab
arvindthoppe <tsarvind996@gmail.com>
2024-02-19 00:58:10 +0400

    change route

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,25 @@
 const puppeteer = require("puppeteer");
 
-async function HTMLtoPDF(fastify, options) {
-  fastify.post("/html-to-pdf", async (request, reply) => {
-    const browser = await puppeteer.launch({
-      headless: true,
-    });
-
-    // Create a new page
-    const page = await browser.newPage();
-    const html = request.body.htmlContent;
-    await page.setContent(html, { waitUntil: "domcontentloaded" });
-
-    await page.emulateMediaType("screen");
-    const pdf = await page.pdf({
-      margin: { top: "100px", right: "52px", bottom: "100px", left: "50px" },
-      printBackground: true,
-      format: "A4",
-    });
-
-    await browser.close();
-
-    reply.code(200).send({ pdf: pdf.toString("base64") });
+fastify.post("/html-to-pdf", async (request, reply) => {
+  const browser = await puppeteer.launch({
+    headless: true,
   });
-}
+
+  // Create a new page
+  const page = await browser.newPage();
+  const html = request.body.htmlContent;
+  await page.setContent(html, { waitUntil: "domcontentloaded" });
+
+  await page.emulateMediaType("screen");
+  const pdf = await page.pdf({
+    margin: { top: "100px", right: "52px", bottom: "100px", left: "50px" },
+    printBackground: true,
+    format: "A4",
+  });
+
+  await browser.close();
+
+  reply.code(200).send({ pdf: pdf.toString("base64") });
+});
 
 module.exports = HTMLtoPDF;
